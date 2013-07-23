270ed636fe9111be9e9eb899f8f07058e61e8e0c
Thomas M. Vogel <vogel@startplus.de>
2013-07-23 22:21:03 +0200

    CmdLet for Nic und CD-ROM added

--- ProfitBricksPSmoduleSoapAPI/CmdLet/cdrom.cs
+++ ProfitBricksPSmoduleSoapAPI/CmdLet/cdrom.cs
@@ -7,7 +7,68 @@ using System.Text;
 
 namespace ProfitBricksPSmoduleSoapAPI.CmdLet
 {
-    class CDRom
+    #region Join_PBRomdriveConnection
+    [Cmdlet(VerbsCommon.Join, "PBRomdriveConnection")]
+    public class Join_PBRomdriveConnection : PBapiPSCmdlet
     {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public string imageId;
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true
+        )]
+        public string serverId;
+
+        [Parameter(
+            Position = 2,
+            Mandatory = false
+        )]
+        public int deviceNumber;
+
+        protected override void ProcessRecord()
+        {
+            romDriveRequest Request = new romDriveRequest();
+            Request.imageId = imageId;
+            Request.serverId = serverId;
+            Request.deviceNumber = deviceNumber;
+            if (deviceNumber != 0)
+            {
+                Request.deviceNumberSpecified = true;
+            }  
+            this.WriteObject(PBApi.Servive.addRomDriveToServer(Request));
+        }
     }
+    #endregion
+
+    #region Remove_PBRomdriveConnection
+    [Cmdlet(VerbsCommon.Remove, "PBRomdriveConnection")]
+    public class Remove_PBRomdriveConnection : PBapiPSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public string imageId;
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true
+        )]
+        public string serverId;
+
+        protected override void ProcessRecord()
+        {
+            this.WriteObject(PBApi.Servive.removeRomDriveFromServer(imageId, serverId));
+        }
+    }
+    #endregion
+
 }
--- ProfitBricksPSmoduleSoapAPI/CmdLet/nic.cs
+++ ProfitBricksPSmoduleSoapAPI/CmdLet/nic.cs
@@ -7,14 +7,152 @@ using System.Text;
 
 namespace ProfitBricksPSmoduleSoapAPI.CmdLet
 {
+    #region New_PBNic
+    [Cmdlet(VerbsCommon.New, "PBNic")]
+    public class New_PBNic : PBapiPSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true
+        )]
+        public string serverId;
 
+        [Parameter(
+            Position = 1,
+            Mandatory = true
+        )]
+        public int lanId;
 
-    
-    
-    
-//Create NIC
-//Get NIC
-//Set Internet Access
+        [Parameter(
+            Position = 2,
+            Mandatory = false
+        )]
+        public string ip;
+
+        [Parameter(
+            Position = 3,
+            Mandatory = false
+        )]
+        public string nicName;
+
+        protected override void ProcessRecord()
+        {
+            createNicRequest Request = new createNicRequest();
+            Request.serverId = serverId;
+            Request.lanId = lanId;
+            Request.ip = ip;
+            Request.nicName = nicName;
+            this.WriteObject(PBApi.Servive.createNic(Request));
+        }
+    }
+    #endregion
+
+    #region Get_PBNic
+    [Cmdlet(VerbsCommon.Get, "PBNic")]
+    public class Get_PBNic : PBapiPSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public string nicId;
+
+        protected override void ProcessRecord()
+        {
+            this.WriteObject(PBApi.Servive.getNic(nicId));
+        }
+    }
+    #endregion
+
+    #region Remove_PBNic
+    [Cmdlet(VerbsCommon.Remove, "PBNic")]
+    public class Remove_PBNic : PBapiPSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true,
+            ValueFromPipeline = true,
+            ValueFromPipelineByPropertyName = true
+        )]
+        public string nicId;
+
+        protected override void ProcessRecord()
+        {
+            this.WriteObject(PBApi.Servive.deleteNic(nicId));
+        }
+    }
+    #endregion
+
+    #region Set_PBNic
+    [Cmdlet(VerbsCommon.Set, "PBNic")]
+    public class Set_PBNic : PBapiPSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true
+        )]
+        public string nicId;
+
+        [Parameter(
+            Position = 1,
+            Mandatory = false
+        )]
+        public int lanId;
+
+        [Parameter(
+            Position = 2,
+            Mandatory = false
+        )]
+        public string ip;
+
+        [Parameter(
+            Position = 3,
+            Mandatory = false
+        )]
+        public string nicName;
+
+        protected override void ProcessRecord()
+        {
+            updateNicRequest Request = new updateNicRequest();
+            Request.nicId = nicId;
+            Request.lanId = lanId;
+            Request.ip = ip;
+            Request.nicName = nicName;
+            this.WriteObject(PBApi.Servive.updateNic(Request));
+        }
+    }
+    #endregion    
+
+    #region Set_PBInternetAccess
+    [Cmdlet(VerbsCommon.Set, "PBInternetAccess")]
+    public class Set_PBInternetAccess : PBapiPSCmdlet
+    {
+        [Parameter(
+            Position = 0,
+            Mandatory = true
+        )]
+        public string datacenterId;
+
+        [Parameter(
+            Position = 1,
+            Mandatory = true
+        )]
+        public int lanId;
+
+        [Parameter(
+            Position = 2,
+            Mandatory = false
+        )]
+        public bool internetAccess;
+
+        protected override void ProcessRecord()
+        {
+            this.WriteObject(PBApi.Servive.setInternetAccess(datacenterId, lanId, internetAccess));
+        }
+    }
+    #endregion    
 //Update NIC
-//Delete NIC
+
 }
